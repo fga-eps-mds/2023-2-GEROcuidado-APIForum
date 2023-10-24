c2b7b0af899d01f23e7f1b7802989fbc41b7052a
Henrique Melo <hacmelo@gmail.com>
2023-10-24 17:00:12 -0300

    (#136) adiciona configuracoes e testes

---
+++ src/autenticacao.guard.spec.ts
@@ -0,0 +1,76 @@
+import { UnauthorizedException } from '@nestjs/common';
+import { Reflector } from '@nestjs/core';
+import { Test, TestingModule } from '@nestjs/testing';
+import { of } from 'rxjs';
+import { AutenticacaoGuard } from './autenticacao.guard';
+
+describe('AutenticacaoGuard', () => {
+  let guard: AutenticacaoGuard;
+  let reflector: Reflector;
+
+  const mockClientProxy = {
+    send: jest.fn(),
+  };
+
+  const mockContext = {
+    switchToHttp: jest.fn().mockReturnValue({
+      getRequest: jest.fn().mockReturnValue({
+        headers: {
+          authorization: 'Bearer token',
+        },
+      }),
+    }),
+    getHandler: jest.fn(),
+    getClass: jest.fn(),
+  };
+
+  beforeEach(async () => {
+    const module: TestingModule = await Test.createTestingModule({
+      providers: [
+        AutenticacaoGuard,
+        {
+          provide: 'AUTH_CLIENT',
+          useValue: mockClientProxy,
+        },
+        Reflector,
+      ],
+    }).compile();
+
+    guard = module.get<AutenticacaoGuard>(AutenticacaoGuard);
+    reflector = module.get<Reflector>(Reflector);
+  });
+
+  it('should be defined', () => {
+    expect(guard).toBeDefined();
+  });
+
+  it('should pass if route is public', async () => {
+    jest.spyOn(reflector, 'getAllAndOverride').mockReturnValue(true);
+
+    const result = await guard.canActivate(mockContext as any);
+
+    expect(result).toBe(true);
+  });
+
+  it('should pass if authentication is successful', async () => {
+    jest.spyOn(reflector, 'getAllAndOverride').mockReturnValue(false);
+    mockClientProxy.send.mockReturnValue(of(true));
+
+    const result = await guard.canActivate(mockContext as any);
+
+    expect(result).toBe(true);
+  });
+
+  it('should not pass if authentication is unsuccessful', async () => {
+    jest.spyOn(reflector, 'getAllAndOverride').mockReturnValue(false);
+    mockClientProxy.send.mockReturnValue(of(false));
+
+    guard
+      .canActivate(mockContext as any)
+      .catch((err) =>
+        expect(err).toEqual(
+          new UnauthorizedException('Usuário não autenticado!'),
+        ),
+      );
+  });
+});

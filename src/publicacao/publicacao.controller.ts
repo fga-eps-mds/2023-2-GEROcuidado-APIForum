1f993e7362f8b0c83661bf0c12f23024554285fa
Thefast777 <221021984@aluno.unb.br>
2023-10-30 20:03:43 -0300

    (#157) cria rota get Co-authored-by: Lucas Martins <martin5.luca5.univ@gmail.com>

--- src/publicacao/publicacao.controller.ts
+++ src/publicacao/publicacao.controller.ts
@@ -1,7 +1,9 @@
-import { Body, Controller, Get, Post } from '@nestjs/common';
+import { Body, Controller, Get, Param, Patch, Post } from '@nestjs/common';
 import { HttpResponse } from '../shared/classes/http-response';
 import { PublicRoute } from '../shared/decorators/public-route.decorator';
+import { IdValidator } from '../shared/validators/id.validator';
 import { CreatePublicacaoDto } from './dto/create-publicacao.dto';
+import { UpdatePublicacaoDto } from './dto/update-publicacao.dto';
 import { Publicacao } from './entities/publicacao.entity';
 import { PublicacaoService } from './publicacao.service';
 
@@ -15,13 +17,25 @@ export class PublicacaoController {
     return 'Aqui deveriamos retornar uma funcao do service ';
   }
 
+  @Get(':id')
+  async findOne(@Param() param: IdValidator): Promise<Publicacao> {
+    return this._service.findOne(param.id);
+  }
+
+  @Patch(':id')
+  async update(
+    @Param() param: IdValidator,
+    @Body() body: UpdatePublicacaoDto,
+  ): Promise<Response<Publicacao>> {
+    const updated = await this._service.update(param.id, body);
+    return new HttpResponse<Publicacao>(updated).onUpdated();
+  }
+
   @Post()
   async create(@Body() body: CreatePublicacaoDto) {
     const created = await this._service.create(body);
     return new HttpResponse<Publicacao>(created).onCreated();
   }
-
-  // Criar uma rota para criar uma Publicacao
 
   // Criar uma rota para remover uma Publicacao
 
--- src/publicacao/publicacao.service.ts
+++ src/publicacao/publicacao.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { CreatePublicacaoDto } from './dto/create-publicacao.dto';
+import { UpdatePublicacaoDto } from './dto/update-publicacao.dto';
 import { Publicacao } from './entities/publicacao.entity';
 
 @Injectable()
@@ -15,4 +16,10 @@ export class PublicacaoService {
     const publicacao = new Publicacao(body);
     return this._repository.save(publicacao);
   }
+
+  async findOne(id: number) {
+    const publicacao = await this._repository.findOneOrFail({ where: { id } });
+    return publicacao;
+  }
+  async update(id: number, body: UpdatePublicacaoDto): Promise<Publicacao> {}
 }
